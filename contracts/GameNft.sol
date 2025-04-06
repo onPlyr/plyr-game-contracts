36d628a3111dec7267b1399833b9b9173426c6d6
lolieatapple <lolieatapple@gmail.com>
2025-04-06 21:53:14 +0800

    Add GameNft and GameNftFactory contracts for NFT minting and management

---
+++ contracts/GameNft.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
+
+contract GameNft is OwnableUpgradeable, ERC721URIStorageUpgradeable {
+    string public gameId;
+    uint256 public totalSupply;
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(address _owner, string memory _gameId, string memory _name, string memory _symbol) public initializer {
+        __Ownable_init(_owner);
+        __ERC721_init(_name, _symbol);
+        __ERC721URIStorage_init();
+        gameId = _gameId;
+    }
+
+    function mint(address _to, string memory _tokenURI) public onlyOwner returns (uint256) {
+        uint256 tokenId = (totalSupply + 1);
+        totalSupply = tokenId;
+        _safeMint(_to, tokenId);
+        _setTokenURI(tokenId, _tokenURI);
+        return tokenId;
+    }
+
+    function burn(uint256 _tokenId) public onlyOwner {
+        _burn(_tokenId);
+        totalSupply -= 1;
+    }
+
+    function gameTransfer(address _from, address _to, uint256 _tokenId) public onlyOwner {
+        _safeTransfer(_from, _to, _tokenId, "");
+    }
+
+    function tokenURI(uint256 tokenId) public view override(ERC721URIStorageUpgradeable) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721URIStorageUpgradeable) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+}
---
+++ contracts/GameNftFactory.sol
@@ -0,0 +1,103 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts/proxy/Clones.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./GameNft.sol";
+
+contract GameNftFactory is OwnableUpgradeable {
+    address public nftImplementation;
+    address public operator;
+    uint256 public totalNftCount;
+
+    mapping(address => string) public nftGameId; // nftAddress => gameId
+    mapping(uint256 => address) public nfts; // nftId => nftAddress
+    mapping(address => uint256) public nftHolderCount; // nftAddress => holderCount
+
+    modifier onlyOperator() { 
+        require(msg.sender == operator, "GameNftFactory: caller is not the game router");
+        _;
+    }
+
+    event NftCreated(address indexed nft, string gameId, string name, string symbol);
+    
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+    
+    function initialize(address _implementation, address _owner, address _operator) public initializer {
+        __Ownable_init(_owner);
+        nftImplementation = _implementation;
+        operator = _operator;
+    }
+
+    function createNft(string memory _gameId, string memory _name, string memory _symbol) external onlyOperator {
+        address nft = Clones.clone(nftImplementation);
+        GameNft(nft).initialize(address(this), _gameId, _name, _symbol);
+        nftGameId[nft] = _gameId;
+        nfts[totalNftCount] = nft;
+        totalNftCount++;
+        emit NftCreated(nft, _gameId, _name, _symbol);
+    }
+
+    function mint(address _nft, address _to, string memory _tokenURI) public onlyOperator {
+        if (IERC20(_nft).balanceOf(_to) == 0) {
+            nftHolderCount[_nft]++;
+        }
+        
+        GameNft(_nft).mint(_to, _tokenURI);
+    }
+
+    function batchMint(address[] memory _nft, address[] memory _to, string[] memory _tokenURI) external onlyOperator {
+        for (uint256 i = 0; i < _to.length; i++) {
+            mint(_nft[i], _to[i], _tokenURI[i]);
+        }
+    }
+
+    function burn(address _nft, address _from, uint256 _tokenId) public onlyOperator {
+        GameNft(_nft).burn(_tokenId);
+
+        if (IERC20(_nft).balanceOf(address(_from)) == 0) {
+            nftHolderCount[_nft]--;
+        }
+    }
+
+    function batchBurn(address[] memory _nft, address[] memory _from, uint256[] memory _tokenId) external onlyOperator {
+        for (uint256 i = 0; i < _from.length; i++) {
+            burn(_nft[i], _from[i], _tokenId[i]);
+        }
+    }
+
+    function gameTransfer(address _nft, address _from, address _to, uint256 _tokenId) public onlyOperator {
+        if (IERC20(_nft).balanceOf(_to) == 0) {
+            nftHolderCount[_nft]++;
+        }
+
+        GameNft(_nft).gameTransfer(_from, _to, _tokenId);
+
+        if (IERC20(_nft).balanceOf(_from) == 0) {
+            nftHolderCount[_nft]--;
+        }
+    }
+
+    function batchGameTransfer(address[] memory _nft, address[] memory _from, address[] memory _to, uint256[] memory _tokenId) external onlyOperator {
+        for (uint256 i = 0; i < _from.length; i++) {
+            gameTransfer(_nft[i], _from[i], _to[i], _tokenId[i]);
+        }
+    }
+
+    function nftInfo(address _nft) external view returns(address tokenAddress, string memory gameId, string memory name, string memory symbol, uint256 totalSupply, uint256 holderCount) {
+        return (address(_nft), nftGameId[_nft], GameNft(_nft).name(), GameNft(_nft).symbol(), GameNft(_nft).totalSupply(), nftHolderCount[_nft]);
+    }
+
+    function configOperator(address _operator) external onlyOwner {
+        operator = _operator;
+    }
+
+    function configNftImplementation(address _implementation) external onlyOwner {
+        nftImplementation = _implementation;
+    }
+}
+
